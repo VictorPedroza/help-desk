42219936723ec89791d1616c11614c2ecc7d2776
VictorPedroza <Victor242206@gmail.com>
2025-06-16 16:45:10 -0300

    feat: Update server response message for root endpoint

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,10 @@
 const express = require("express")
-
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello World! The API is Running")
+    res.send("API is Running")
 })
 
 app.listen(8088, () => {
-    console.log("API is Running!")
+    console.log(`API is Running!`)
 })

95867cb15394289934105e74d2e1064539816d09
VictorPedroza <Victor242206@gmail.com>
2025-06-25 17:22:03 -0300

    feat: Configure CORS middleware with specific options for enhanced API security

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -9,7 +9,12 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("API is Running");
